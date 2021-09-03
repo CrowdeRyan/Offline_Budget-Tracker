1b5d5bc83d3f40430afd25c220009e29f5e607a3
Ryan Crowder <crowderyan@gmail.com>
2021-09-03 05:56:44 -0400

    added activation event

--- public/service-worker.js
+++ public/service-worker.js
@@ -24,3 +24,21 @@ self.addEventListener("install", function (evt) {
 
   self.skipWaiting();
 });
+
+// activate
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
