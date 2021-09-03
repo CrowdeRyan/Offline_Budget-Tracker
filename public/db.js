a11b9a0053cac35d23369f776f6091bc5d960b4a
Ryan Crowder <crowderyan@gmail.com>
2021-09-03 05:50:19 -0400

    added db readwrite

--- public/db.js
+++ public/db.js
@@ -20,3 +20,14 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
   console.log("Error encountered " + event.target.errorCode);
 };
+
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+
+  // add record to your store with add method.
+  store.add(record);
+}
