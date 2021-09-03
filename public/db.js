3ba41831731fecfbfee82f83eada6f18bc3454b1
Ryan Crowder <crowderyan@gmail.com>
2021-09-03 05:51:38 -0400

    added initial db function

--- public/db.js
+++ public/db.js
@@ -32,5 +32,14 @@ function saveRecord(record) {
   store.add(record);
 }
 
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
+  // access your pending object store
+  const store = transaction.objectStore("pending");
+  // get all records from store and set to a variable
+  const getAll = store.getAll();
+}
+
 // this listens for the app coming back online
 window.addEventListener("online", checkDatabase);
