4955173444ac709b62c9816604e75c5d102c4e2e
Ryan Crowder <crowderyan@gmail.com>
2021-09-03 05:48:20 -0400

    added network check

--- public/db.js
+++ public/db.js
@@ -7,3 +7,12 @@ request.onupgradeneeded = function (event) {
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
