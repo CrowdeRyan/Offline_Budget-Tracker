90ac3db354bc85b2d2497c9ca2783e686a2313ad
Ryan Crowder <crowderyan@gmail.com>
2021-09-03 05:47:57 -0400

    added objectStore with upgrading

--- public/db.js
+++ public/db.js
@@ -1,3 +1,9 @@
 let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
