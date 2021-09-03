217c7e15eebfadd45fc1ac5d5d0933100e96bb0b
Ryan Crowder <crowderyan@gmail.com>
2021-09-03 05:55:06 -0400

    linking icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,5 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+const iconSizes = ["192", "512"];
+const iconFiles = iconSizes.map((size) => `/icons/icon-${size}x${size}.png`);
