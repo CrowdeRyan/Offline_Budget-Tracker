1552be2123e240094c6a9cd559614182a073cb19
Ryan Crowder <crowderyan@gmail.com>
2021-09-03 05:55:39 -0400

    establishing static cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,3 +3,12 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 const iconSizes = ["192", "512"];
 const iconFiles = iconSizes.map((size) => `/icons/icon-${size}x${size}.png`);
+
+const staticFilesToPreCache = [
+  "/",
+  "/index.js",
+  "/db.js",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/styles.css",
+].concat(iconFiles);
