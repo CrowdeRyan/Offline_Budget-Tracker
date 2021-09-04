0be121bfa1be0dc4f47d4d9c4b3713504501a0ca
Ryan Crowder <crowderyan@gmail.com>
2021-09-04 06:03:06 -0400

    fixed naming errors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// server file used to initate the ports
+
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/progressive-budget",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
